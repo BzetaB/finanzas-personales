b7336ca1491f9091281f4967134e69857473729b
bzetab <zevallosbenjamin100@gmail.com>
2025-10-18 15:02:54 -0500

    feat(account-controller): add endpoint to get account by name with parameter validation

--- src/main/java/com/bzetab/finanzaspersonales/controllers/AccountController.java
+++ src/main/java/com/bzetab/finanzaspersonales/controllers/AccountController.java
@@ -4,14 +4,14 @@ import com.bzetab.finanzaspersonales.models.dto.AccountDto;
 import com.bzetab.finanzaspersonales.utils.exceptions.model.SuccessResponse;
 import com.bzetab.finanzaspersonales.services.AccountService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Validated
 @RequestMapping("api/v1/account")
 public class AccountController {
     private final AccountService accountService;
@@ -27,6 +27,16 @@ public class AccountController {
                         .message("Account created successfully")
                         .status(HttpStatus.CREATED.value())
                         .data(accountService.createAccount(accountDto))
+                        .build());
+    }
+
+    @GetMapping
+    public ResponseEntity<SuccessResponse> findAccountByName(@NotBlank(message = "Name is required") @RequestParam("name") String name ){
+        return ResponseEntity.ok(
+                SuccessResponse.builder()
+                        .message("Account found successfully")
+                        .status(HttpStatus.OK.value())
+                        .data(accountService.findByName(name))
                         .build());
     }
 }
