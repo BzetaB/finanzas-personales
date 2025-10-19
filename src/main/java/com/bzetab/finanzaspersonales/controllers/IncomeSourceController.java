14ea29dc2df13a059b5a8e6c9c043d4649d0b5a3
bzetab <zevallosbenjamin100@gmail.com>
2025-10-19 12:25:24 -0500

    feat(income-source-controller): add endpoint to get income source by name with parameter validation

--- src/main/java/com/bzetab/finanzaspersonales/controllers/IncomeSourceController.java
+++ src/main/java/com/bzetab/finanzaspersonales/controllers/IncomeSourceController.java
@@ -4,12 +4,10 @@ import com.bzetab.finanzaspersonales.models.entities.IncomeSource;
 import com.bzetab.finanzaspersonales.services.IncomeSourceService;
 import com.bzetab.finanzaspersonales.utils.exceptions.model.SuccessResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/income-source")
@@ -27,6 +25,16 @@ public class IncomeSourceController {
                         .status(HttpStatus.CREATED.value())
                         .message("Income source created successfully")
                         .data(incomeSourceService.createIncomeSource(incomeSource))
+                        .build());
+    }
+
+    @GetMapping
+    public ResponseEntity<SuccessResponse> findIncomeSourceByName(@NotBlank(message = "Name is required") @RequestParam("name") String name ){
+        return ResponseEntity.ok(
+                SuccessResponse.builder()
+                        .message("Account found successfully")
+                        .status(HttpStatus.OK.value())
+                        .data(incomeSourceService.findByName(name))
                         .build());
     }
 
